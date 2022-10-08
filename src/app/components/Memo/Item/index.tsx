f2ad426a01642a0fe276fdbe9b3d5231ba437bf5
tintoll <belselios@gmail.com>
2022-10-09 08:37:07 +0900

    메모앱 - 메모리스트 안의 메모아이템 컴포넌트 작성

---
+++ src/app/components/Memo/Item/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div<{ selected?: boolean }>`
+  width: 100%;
+  height: fit-content;
+  padding: 8px 15px;
+  margin: 5px 0;
+  border-radius: 5px;
+  border-bottom: ${props => (props.selected ? 'none' : '1px solid #e9e9e9')};
+  user-select: none;
+  cursor: pointer;
+  background-color: ${props => (props.selected ? '#ffe48b' : '#fff')};
+`;
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  font-weight: 700;
+  color: #2c2c2c;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+const MemoContent = styled.div`
+  font-size: 0.85rem;
+  color: #8b8b8b;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+export default function MemoItem({
+  id,
+  preview,
+  create_at,
+  selected,
+}: {
+  id: string;
+  preview: string;
+  create_at: string;
+  selected: boolean;
+}) {
+  return (
+    <Box selected={selected}>
+      <MemoTitle>{preview}</MemoTitle>
+      <MemoContent>{new Date(create_at).toLocaleString('ko')}</MemoContent>
+      <MemoContent>{preview}</MemoContent>
+    </Box>
+  );
+}
--- src/app/components/Memo/List/index.tsx
+++ src/app/components/Memo/List/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
+import MemoItem from '../Item';
 
 const List = styled.div`
   width: 300px;
   height: calc(100vh - 60px);
   border-right: 1px solid #e9e9e9;
   padding: 0 10px;
+
+  @media (max-width: 687px) {
+    margin-left: -200px;
+    transition: 0.2s;
+    &:hover {
+      margin-left: 0;
+    }
+  }
 `;
 export default function MemoList() {
-  return <List></List>;
+  return (
+    <List>
+      <MemoItem
+        id="1"
+        create_at={new Date().toString()}
+        preview="나는 메모앱입니다."
+        selected={true}
+      />
+      <MemoItem
+        id="1"
+        create_at={new Date().toString()}
+        preview="나는 메모앱입니dspepsk dsjeopsdk sdioe  "
+        selected={false}
+      />
+      <MemoItem
+        id="1"
+        create_at={new Date().toString()}
+        preview="나는 메모앱입니다."
+        selected={false}
+      />
+    </List>
+  );
 }
